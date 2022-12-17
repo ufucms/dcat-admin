189d04e163c694a5279fe6683afb20ec2cebc019
Eric <liushun@aliyun.com>
2022-12-17 23:16:48 +0800

    修复markdown样式

--- src/Grid/Displayers/ModalMarkdown.php
+++ src/Grid/Displayers/ModalMarkdown.php
@@ -2,18 +2,18 @@
 
 namespace Dcat\Admin\Grid\Displayers;
 
-use GrahamCampbell\Markdown\Facades\Markdown;
+use Dcat\Admin\Widgets\Markdown;
 
 class ModalMarkdown extends Modal
 {
     public function display($field = null, $icon = null)
     {
         if($field === null){
-            $content = Markdown::convert($this->row->content)->getContent();
+            $content = Markdown::make($this->row->content);
             $this->title = admin_trans_field('content');
         }else{
             $this->title = admin_trans_field($field);
-            $content = Markdown::convert($this->row->$field)->getContent();
+            $content = Markdown::make($this->row->$field);
         }
         $icon ? $this->icon = $icon : null;
         return parent::display(function ($modal) use($content) {
